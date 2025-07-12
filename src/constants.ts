987957392a94b2a660db63181170546bfd51e727
Andrew Schwegler <schwegler@gmail.com>
2025-07-12 00:56:32 -0500

    init

--- src/constants.ts
+++ src/constants.ts
@@ -1,46 +1,132 @@
 import { Label } from './types.js';
 
-export const DELETE = 'insert-rkey-of-delete-post-here';
+export const DELETE = '3ltqmdjvkre2y';
 export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'earth',
+    rkey: '3ltqndkgvwv2f',
+    identifier: 'ava',
     locales: [
-      { lang: 'en', name: 'Earth 🌎', description: 'Earth'},
-      { lang: 'pt-BR', name: 'Terra 🌎', description: 'Terra'},
+      { lang: 'en', name: 'Ava', description: 'Ready for an aura painting'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'fire',
+    rkey: '3ltqndkjsyd2o',
+    identifier: 'adrian',
     locales: [
-      { lang: 'en', name: 'Fire 🔥', description: 'Fire'},
-      { lang: 'pt-BR', name: 'Fogo 🔥', description: 'Fogo'},
+      { lang: 'en', name: 'Adrian', description: 'Rooting for the greatest big brother player of all time'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'air',
+    rkey: '3ltqndkmnuf2o',
+    identifier: 'amy',
     locales: [
-      { lang: 'en', name: 'Air 💨', description: 'Air'},
-      { lang: 'pt-BR', name: 'Ar 💨', description: 'Ar'},
+      { lang: 'en', name: 'Amy', description: 'Rooting for the shiny pokemon of the house'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'water',
+    rkey: '3ltqndkppqk2j',
+    identifier: 'ashley',
     locales: [
-      { lang: 'en', name: 'Water 💧', description: 'Water'},
-      { lang: 'pt-BR', name: 'Água 💧', description: 'Água'},
+      { lang: 'en', name: 'Ashley', description: 'Blowing up Fiji is harder than passing the bar'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'love',
+    rkey: '3ltqndkslt22l',
+    identifier: 'jimmy',
     locales: [
-      { lang: 'en', name: 'Love 💞', description: 'Love'},
-      { lang: 'pt-BR', name: 'Amor 💞', description: 'Amor'},
+      { lang: 'en', name: 'Jimmy', description: 'Please be Ragan 2.0'},
+    ]
+  },
+  {
+    rkey: '3ltqndkvmdf2y',
+    identifier: 'kat',
+    locales: [
+      { lang: 'en', name: 'Katherine', description: "I'm a raging feminist"},
+    ]
+  },
+  {
+    rkey: '3ltqndkyj352l',
+    identifier: 'zae',
+    locales: [
+      { lang: 'en', name: 'Zae', description: "I have tried alot of new things since leaving the chirch."},
+    ]
+  },
+  {
+    rkey: '3ltqndlaeff2f',
+    identifier: 'zach',
+    locales: [
+      { lang: 'en', name: 'Zach', description: "My christianity is really important to me"},
+    ]
+  },
+  {
+    rkey: '3ltqndldpcj24',
+    identifier: 'will',
+    locales: [
+      { lang: 'en', name: 'Will', description: "All aboard with Captain Will's Gamecock podcast or whatever! :D"},
+    ]
+  },
+  {
+    rkey: '3ltqndlgvpf2o',
+    identifier: 'vince',
+    locales: [
+      { lang: 'en', name: 'Vince', description: "Frist job: delivery guy, Second job: HoH"},
+    ]
+  },
+  {
+    rkey: '3ltqndljrh32o',
+    identifier: 'rylie',
+    locales: [
+      { lang: 'en', name: 'Rylie', description: "Probably didn't need to make this one..."},
+    ]
+  },
+  {
+    rkey: '3ltqndlmmxv2f',
+    identifier: 'rachel',
+    locales: [
+      { lang: 'en', name: 'Rachel', description: "Floaters, grab a life vest!"},
+    ]
+  },
+  {
+    rkey: '3ltqndlpm6q2u',
+    identifier: 'morgan',
+    locales: [
+      { lang: 'en', name: 'Morgan', description: "Might be a gamer"},
+    ]
+  },
+  {
+    rkey: '3ltqndlsgjf2f',
+    identifier: 'mickey',
+    locales: [
+      { lang: 'en', name: 'Mickey', description: "I'm rooting for Mickey"},
+    ]
+  },
+  {
+    rkey: '3ltqndlvlns2j',
+    identifier: 'lauren',
+    locales: [
+      { lang: 'en', name: 'Lauren', description: "Big fan Big showmance possibility"},
+    ]
+  },
+  {
+    rkey: '3ltqndlyqjk2l',
+    identifier: 'kelley',
+    locales: [
+      { lang: 'en', name: 'Kelley', description: "Kelley is the first houseguest from South Dakota."},
+    ]
+  },
+  {
+    rkey: '3ltqndm3pir2a',
+    identifier: 'keanu',
+    locales: [
+      { lang: 'en', name: 'Keanu', description: "I've found my Ted!"},
+    ]
+  },
+    {
+    rkey: '3ltqndm6qcm2o',
+    identifier: 'benice',
+    locales: [
+      { lang: 'en', name: 'Be Nice!', description: "Please be nice in my replies."},
     ]
   },
 ];
